5010f4cf60b1e3d470ea844260947851b9298d44
hyunkiki <rlagusrl98@gmail.com>
2023-11-22 22:59:00 +0900

    11.22 업데이트

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -54,3 +54,39 @@ gsap.utils.toArray(".reveal").forEach((item) => {
     },
   });
 });
+
+let viewHeight = window.innerHeight;
+let viewWidth = window.innerWidth;
+
+let textContainers = document.querySelectorAll(".text-container");
+
+textContainers.forEach((element, index) => {
+  let top = element.getBoundingClientRect().top;
+  let start = viewHeight - top;
+
+  let firstText = element.querySelector(".parallax-text:first-child");
+  let secondText = element.querySelector(".parallax-text:last-child");
+
+  gsap.to(firstText, {
+    scrollTrigger: {
+      trigger: element,
+      scrub: true,
+      start: start + "px bottom",
+      end: "bottom top",
+    },
+    x: "-54vw",
+    transformOrigin: "left center",
+    ease: "none",
+  });
+  gsap.to(secondText, {
+    scrollTrigger: {
+      trigger: element,
+      scrub: true,
+      start: start + "px bottom",
+      end: "bottom top",
+    },
+    x: "32vw",
+    transformOrigin: "left center",
+    ease: "none",
+  });
+});
