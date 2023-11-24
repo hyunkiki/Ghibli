e24b69f0c689ce50a4f899b18992b58b214d52d2
hyunkiki <rlagusrl98@gmail.com>
2023-11-24 20:21:05 +0900

    11.24 업데이트

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -90,3 +90,51 @@ textContainers.forEach((element, index) => {
     ease: "none",
   });
 });
+
+gsap.to(".left-img", {
+  yPercent: -30,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".art-wrapper .art-wrapper2",
+    scrub: 1,
+  },
+});
+gsap.to(".right-img", {
+  yPercent: -10,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".art-wrapper .art-wrapper2",
+    scrub: 1,
+  },
+});
+gsap.to(".titel", {
+  yPercent: -0,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".art-wrapper .art-wrapper2",
+    scrub: 1,
+    end: "100px;",
+  },
+});
+gsap.to(".left-scroll", {
+  xPercent: -100,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".artist",
+    scrub: 1,
+    start: "-50%",
+    pin: true,
+    end: "+=1000",
+  },
+});
+gsap.to(".right-scroll", {
+  xPercent: 100,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".artist",
+    scrub: 1,
+    pin: true,
+    start: "-50%",
+    end: "+=1000",
+  },
+});
