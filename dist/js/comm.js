77860dbf55dd246795d28ea5324c36164bc826cd
hyunkiki <rlagusrl98@gmail.com>
2023-11-30 22:14:51 +0900

    11-30 업데이트

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -138,3 +138,22 @@ gsap.to(".right-scroll", {
     end: "+=1000",
   },
 });
+
+gsap.registerPlugin(ScrollTrigger);
+
+let horizontalSection = document.querySelector(".horizontal");
+
+console.log(horizontalSection.scrollWidth);
+
+gsap.to(".horizontal", {
+  x: () => horizontalSection.scrollWidth * -1,
+  xPercent: 100,
+  scrollTrigger: {
+    trigger: ".horizontal",
+    start: "center center",
+    end: "+=2000px",
+    pin: "#horizontal-scoll",
+    scrub: true,
+    invalidateOnRefresh: true,
+  },
+});
