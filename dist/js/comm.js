b6485834c6a184c7e3b8cf8074ccd82323f11374
hyunkiki <rlagusrl98@gmail.com>
2024-06-24 16:56:23 +0900

    06-24 업데이트

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -166,3 +166,13 @@ gsap.to(".horizontal", {
 //     $("html, body").animate({ scrollTop: $(this.hash).offset.top }, 300);
 //   });
 // });
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+}); //header에 메뉴의 a태그를 누르면 해당영역으로 부드럽게 이동하기
